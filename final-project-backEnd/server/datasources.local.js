705718d13086fb04092de56335e55d39cd005f69
pi <payam.farah@live.com>
2020-07-20 19:38:05 -0700

    added environment variables

---
+++ final-project-backEnd/server/datasources.local.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+    db: {
+        name: "db",
+        connector: "memory"
+    },
+    mongo: {
+        url: process.env.DB_URL, 
+        connector: 'mongodb',
+        name: "mongo"
+    }
+}
